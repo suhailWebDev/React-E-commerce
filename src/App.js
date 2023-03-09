5096bc309629b006f727f69e3018763a405f3ede
Mohd Suhail <mdsuhail1410@gmail.com>
2023-03-09 12:10:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap-grid.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import Product from './Components/Product';
+import Cart from './Components/Cart';
+import SingleProduct from './Components/SingleProduct';
+import Header from './Components/Header';
+import Contact from './Components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/products' element={<Product/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/sibgleproduct/:id' element={<SingleProduct/>}/>
+    </Routes>
+    </>
   );
 }
 
---
+++ src/Components/Header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../Images/logo 1.png';
+import {FiShoppingCart} from 'react-icons/fi'
+
+const Header = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg d-flex flex-lg-row flex-sm-column align-items-center">
+  <div className="container-fluid">
+    <Link className="navbar-brand" to="/"><img src={logo} alt='logo' className='logo'/></Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarToggleExternalContent">
+      <ul className="mb-lg-0 mb-sm-0 d-flex align-items-center gap-4 navbar-list ms-auto p-2">
+        <li className="nav-item">
+          <Link className="nav-link active list-link" aria-current="page" to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to='/about' className="nav-link list-link">About</Link>
+        </li>
+        <li className="nav-item">
+          <Link to='/products' className="nav-link list-link">Products</Link>
+        </li>
+        <li className="nav-item">
+          <Link to='/contact' className="nav-link list-link">Contact</Link>
+        </li>
+        <li>
+            <button className='btn' id='btn-login'>Log In</button>
+        </li>
+        <li>
+        <Link to='/cart' className='cart-trolley-link'><FiShoppingCart name="cart" className='cart-trolley'/><span className='cart-total-items'>0</span></Link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    </>
+  )
+}
+
+export default Header
